48472f4328a70e088a269b7221d57fe29e240b43
yo <132378436+Yo-2@users.noreply.github.com>
2026-07-11 11:39:04 +0800

    feat: add menu

---
+++ src/scripts/menu.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector(".menu");
+menu?.addEventListener("click", () => {
+  const isExpanded = menu.getAttribute("aria-expanded") === "true" || false;
+  menu.setAttribute("aria-expanded", `${!isExpanded}`);
+});
